ac6d3fc93bd40a5bb50ac45ee60a6cbc8b6410a4
Rodrigo Pavon <rodrigopavongomez@gmail.com>
2023-07-21 11:48:50 -0300

    Create states for navbar

--- components/layout/navbar/index.tsx
+++ components/layout/navbar/index.tsx
@@ -1,7 +1,22 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import { useState } from 'react';
 
 const Navbar = () => {
+  const [isOpen, setIsOpen]= useState(false)
+  const [isSubmenuOpen, setIsSubmenuOpen] = useState(false)
+
+  const toggleMobileMenu = () => {
+    setIsOpen(!isOpen);
+  }
+
+  const toggleSubmenu = () => {
+    setIsSubmenuOpen(!isSubmenuOpen)
+  }
+
+  const toggleMobileMenuClose = () => {
+    setIsOpen(false)
+  }
     return (
 		<header className="bg-white">
   <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
@@ -15,7 +30,7 @@ const Navbar = () => {
       </Link>
     </div>
     <div className="flex lg:hidden">
-      <button type="button" className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
+      <button type="button" className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700" onClick={toggleMobileMenu}>
         <span className="sr-only">Open main menu</span>
         <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
           <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
@@ -35,7 +50,7 @@ const Navbar = () => {
     </div>
   </nav>
   {/* <!-- Mobile menu, show/hide based on menu open state. --> */}
-  <div className="lg:hidden" role="dialog" aria-modal="true">
+  <div className={`lg:hidden ${isOpen ? 'block' : 'hidden'}`} role="dialog" aria-modal="true">
     {/* <!-- Background backdrop, show/hide based on slide-over state. --> */}
     <div className="fixed inset-0 z-10"></div>
     <div className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
@@ -44,7 +59,7 @@ const Navbar = () => {
           <span className="sr-only">Your Company</span>
           <Image width={100} height={100} className="h-8 w-auto" src="/images/hidraLogo.png" alt=""/>
         </a>
-        <button type="button" className="-m-2.5 rounded-md p-2.5 text-gray-700">
+        <button type="button" className="-m-2.5 rounded-md p-2.5 text-gray-700" onClick={toggleMobileMenuClose}>
           <span className="sr-only">Close menu</span>
           <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
             <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
@@ -55,7 +70,7 @@ const Navbar = () => {
         <div className="-my-6 divide-y divide-gray-500/10">
           <div className="space-y-2 py-6">
             <div className="-mx-3">
-              <button type="button" className="flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50" aria-controls="disclosure-1" aria-expanded="false">
+              <button type="button" className="flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50" aria-controls="disclosure-1" aria-expanded="false" onClick={toggleSubmenu}>
                 Product
                 {/* <!--
                   Expand/collapse icon, toggle classes based on menu open state.
@@ -67,7 +82,7 @@ const Navbar = () => {
                 </svg>
               </button>
               {/* <!-- 'Product' sub-menu, show/hide based on menu state. --> */}
-              <div className="mt-2 space-y-2" id="disclosure-1">
+              <div className={`mt-2 space-y-2  ${isSubmenuOpen ? 'block' : 'hidden'}`} id="disclosure-1">
                 <a href="#" className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50">Analytics</a>
                 <a href="#" className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50">Engagement</a>
                 <a href="#" className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50">Security</a>
