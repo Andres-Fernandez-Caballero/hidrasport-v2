ebc2a7d5f0ae988d72b56a7663160d0f148557ac
Andres Fernandez <andres.fernandezcaballero@davinci.edu.ar>
2023-08-20 12:01:43 -0300

    CREATE tool apiCall

---
+++ tools/apiCall.ts
@@ -0,0 +1,21 @@
+
+type Method = 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH';
+interface ApiCall {
+    url: string;
+    method?: Method;
+    body?: unknown;
+    token?: string;
+}
+
+export async function apiCall<T>({ url, method='GET', body, token }: ApiCall): Promise<T> {
+    const response =await fetch(url, {
+        method,
+        credentials: 'include',
+        headers: {
+            'Content-Type': 'application/json',
+            "Authorization": token? `Token ${token}` : '',
+        },
+        body: JSON.stringify(body),
+    })
+    return await response.json();
+}
