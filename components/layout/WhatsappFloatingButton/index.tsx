d28d586f6f7695c64972cdf7f744849d5f7cc126
Rodrigo Pavon <rodrigopavongomez@gmail.com>
2023-10-06 20:40:41 -0300

    add logic whatsapp

--- components/layout/WhatsappFloatingButton/index.tsx
+++ components/layout/WhatsappFloatingButton/index.tsx
@@ -1,10 +1,15 @@
+import { WHATSAPP_BUSSINESS_NUMBER } from "@config/index";
 import { FaWhatsapp } from "react-icons/fa";
 const WhatsappFloatingButton = () => {
+  const whatsappLink = `https://wa.me/${WHATSAPP_BUSSINESS_NUMBER}`
+
   return (
     <div className="fixed bottom-8 right-8">
+        <a href={whatsappLink}>
       <button className="bg-green-700 hover:bg-green-600 text-white font-bold py-3 px-3 rounded-full">
         <FaWhatsapp width={100} />
       </button>
+      </a>
     </div>
   );
 };
--- config/index.ts
+++ config/index.ts
@@ -1,5 +1,4 @@
-export const WHATSAPP_BUSSINESS_NUMBER =
-  process.env.WHATSAPP_BUSSINESS_NUMBER || "";
+export const WHATSAPP_BUSSINESS_NUMBER =process.env.WHATSAPP_BUSSINESS_NUMBER || "";
 export const AFIP_QR = process.env.AFIP_QR || "";
 export const AFFIP_DATA_WEB = process.env.AFFIP_DATA_WEB || "";
 export const SERVER_URL = process.env.SERVER_URL || "";
