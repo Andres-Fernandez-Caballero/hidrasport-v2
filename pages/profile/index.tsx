278d5520c00c6cf1c8696a34f42eb3073ff6e9d9
Rodrigo Pavon <rodrigopavongomez@gmail.com>
2023-10-07 00:01:23 -0300

    first part of desing page profile

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -4,55 +4,41 @@ import { IoPencil } from "react-icons/io5";
 const Profile = () => {
   const { userSession, logout } = useAuthStore();
   return (
-    <>
-      <section className="container p-4 m-auto">
-        <aside className="flex items-center justify-between">
-          <header>
-            <h2 className="text-gray-700 font-bold text-4xl leading-10 text-justify px-12 py-6">
-              Profile
-            </h2>
-          </header>
-          <aside>
-            <button
-              onClick={logout}
-              className="text-sm font-semibold leading-6 text-gray-900 px-12 py-6"
-            >
-              Logout <i className="fa-solid fa-arrow-right-from-bracket"></i>
-            </button>
-          </aside>
+    <section className="container p-4 m-auto">
+      <aside className="flex items-center justify-between">
+        <header>
+          <h1 className="text-gray-700 font-bold text-4xl leading-10 text-justify px-12 py-6">
+            Profile
+          </h1>
+        </header>
+        <aside>
+          <button
+            onClick={logout}
+            className="text-sm font-semibold leading-6 text-gray-900 px-12 py-6"
+          >
+            Logout <i className="fa-solid fa-arrow-right-from-bracket"></i>
+          </button>
         </aside>
-        <article className="max-w-screen-lg px-4 py-8 m-auto shadow-md bg-white rounded-lg shadow-md border border-gray-700">
-          <div className="flex items-center justify-between">
-            <header>
-              <h2 className="text-gray-700 font-bold text-2xl leading-9">
-                Mis datos
-              </h2>
-            </header>
-          </div>
-
-          <aside className="mt-2">
-            <p className="text-gray-600 text-md leading-6 font-bold">
-              Usuario:{" "}
-              <span className="font-normal italic">{userSession.username}</span>
-            </p>
-            <p className="text-gray-600 text-md leading-6 font-bold">
-              Email:{" "}
-              <span className="font-normal italic">{userSession.email}</span>
-            </p>
-          </aside>
-          <div className="flex items-center justify-between mt-5">
-            <a
-              href="#"
-              className="text-blue-600 dark:text-blue-400 hover:underline leading-6 flex items-center"
-              tabIndex={0}
-              role="link"
-            >
-              Editar perfil <IoPencil className="ml-1" />
-            </a>
-          </div>
-        </article>
-      </section>
-    </>
+      </aside>
+      <article className="block rounded-lg bg-white container max-w-screen-lg m-auto border border-gray-700 border-top-2 border-dark-200">
+        <h2 className="border-b-2 border-dark-200 px-6 py-3 text-xl font-medium leading-tight bg-zinc-950 text-center text-white">
+          Mis Datos
+        </h2>
+        <button className="float-right inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-wide hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition mr-2">
+          Editar <IoPencil />
+        </button>
+        <section className="p-4 mt-2 grid grid-rows-2">
+          <p className="text-zinc-950 text-md leading-6 font-bold">
+            Usuario:{" "}
+            <span className="font-normal italic">{userSession.username}</span>
+          </p>
+          <p className="text-zinc-950 text-md leading-6 font-bold">
+            Email:{" "}
+            <span className="font-normal italic">{userSession.email}</span>
+          </p>
+        </section>
+      </article>
+    </section>
   );
 };
 
