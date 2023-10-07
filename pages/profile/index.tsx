e729eb7871f521d071b7aeb63ca1a5f553dcc17f
Rodrigo Pavon <rodrigopavongomez@gmail.com>
2023-10-07 00:07:04 -0300

    change color button edit profile

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -24,8 +24,8 @@ const Profile = () => {
         <h2 className="border-b-2 border-dark-200 px-6 py-3 text-xl font-medium leading-tight bg-zinc-950 text-center text-white">
           Mis Datos
         </h2>
-        <button className="float-right inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-wide hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition mr-2">
-          Editar <IoPencil />
+        <button className="float-right inline-flex items-center px-4 py-2 bg-cyan-500 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-wide hover:bg-cyan-600 focus:ring ring-gray-300 disabled:opacity-25 transition mr-2">
+          Editar datos <IoPencil />
         </button>
         <section className="p-4 mt-2 grid grid-rows-2">
           <p className="text-zinc-950 text-md leading-6 font-bold">
