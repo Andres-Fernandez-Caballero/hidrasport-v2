9cbee7fe5c587e7ec0bdd7efd62f8d7b7c0ec3f0
Andres Fernandez <andres.fernandezcaballero@davinci.edu.ar>
2023-10-12 21:06:11 -0300

    FIX afip link component

--- components/layout/AFIP/index.tsx
+++ components/layout/AFIP/index.tsx
@@ -1,14 +1,14 @@
-import { AFIP_DATA_WEB, AFIP_QR } from "@config/index"
-import Image from "next/image"
-import Link from "next/link"
+import { AFIP_DATA_WEB, AFIP_QR } from "@config/index";
+import Image from "next/image";
+import Link from "next/link";
 
 const AFIP = () => {
-    return (
-        <section className='col-span-4'>
-            <a href={AFIP_QR} target="_F960AFIPInfo">
-           <img src={AFIP_DATA_WEB} style={{border: '0' }} width={60} height={80} />
-            </a>
-		</section>
-    )
-}
-export default AFIP
+  return (
+    <section className="col-span-4">
+      <Link href={AFIP_QR} target="_F960AFIPInfo">
+        <Image src={AFIP_DATA_WEB} alt="afip-qr" width={60} height={80} />
+      </Link>
+    </section>
+  );
+};
+export default AFIP;
