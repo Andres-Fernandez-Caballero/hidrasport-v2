8c5e8ddaf783679976136c32e9eb2be64bdd38bc
Andres Fernandez <andres.fernandezcaballero@davinci.edu.ar>
2023-10-13 18:55:06 -0300

    REPAIR AFIP TAG

--- components/layout/AFIP/index.tsx
+++ components/layout/AFIP/index.tsx
@@ -1,14 +1,12 @@
-import { AFIP_DATA_WEB, AFIP_QR } from "@config/index";
+import {} from "@config/index";
 import Image from "next/image";
 import Link from "next/link";
-
-const AFIP = () => {
-  return (
-    <section className="col-span-4">
-      <Link href={AFIP_QR} target="_F960AFIPInfo">
-        <Image src={AFIP_DATA_WEB} alt="afip-qr" width={60} height={80} />
-      </Link>
-    </section>
-  );
-};
+import { AFIP_QR, AFIP_QR_IMAGE } from "@config/index";
+const AFIP = () => (
+  <section className="col-span-4">
+    <Link href={AFIP_QR} target="_F960AFIPInfo">
+      <Image src={AFIP_QR_IMAGE} alt="afip-qr" width={60} height={80} />
+    </Link>
+  </section>
+);
 export default AFIP;
--- config/index.ts
+++ config/index.ts
@@ -1,5 +1,5 @@
-export const WHATSAPP_BUSINESS_NUMBER = process.env.WHATSAPP_BUSINESS_NUMBER || "";
-export const AFIP_QR = process.env.AFIP_QR || "";
-export const AFIP_DATA_WEB = process.env.AFIP_DATAWEB || "";
+export const WHATSAPP_BUSINESS_NUMBER =
+  process.env.NEXT_PUBLIC_WHATSAPP_BUSINESS_NUMBER || "";
+export const AFIP_QR = (process.env.NEXT_PUBLIC_AFIP_QR as string) || "";
+export const AFIP_QR_IMAGE = process.env.NEXT_PUBLIC_AFIP_QR_IMAGE || "";
 export const SERVER_URL = process.env.SERVER_URL || "";
-console.log("WhatsApp Business Number:", WHATSAPP_BUSINESS_NUMBER);
