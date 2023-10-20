d49c5228acc9e57a6bbd031f4db62d473e276dc3
Andres Fernandez <andres.fernandezcaballero@davinci.edu.ar>
2023-10-20 00:24:08 -0300

    ADD pagination in products category page

---
+++ pages/productos/index.tsx
@@ -0,0 +1,11 @@
+import ContentMain from "@components/layout/contentMain";
+
+const ProductListPage = () => {
+  return (
+    <ContentMain title="Nuestros Productos">
+      <p>productos</p>
+    </ContentMain>
+  );
+};
+
+export default ProductListPage;
