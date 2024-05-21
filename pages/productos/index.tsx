5e768b7aed53a3a005ee255e87dda956ad7a99a2
Andres Fernandez <andres.fernandezcaballero@davinci.edu.ar>
2024-05-20 23:29:52 -0300

    add pagination

--- pages/productos/index.tsx
+++ pages/productos/index.tsx
@@ -2,36 +2,43 @@
 
 import ContentMain from "@components/layout/contentMain";
 import EmptyProduct from "@components/product/emptyProduct";
-import { ReadonlyURLSearchParams, useSearchParams } from "next/navigation";
-import React from "react";
+import React, { useState } from "react";
 import useSWR from "swr";
 import ProductGridList from "@components/product/productGridList";
 import Loader from "@components/Loader";
+import { useRouter } from "next/router";
 
 const ProductListPage: React.FC = () => {
-  const searchParams = useSearchParams();
-  const page = searchParams.get("page") || "2";
 
-  const nextPage = (searchParams: ReadonlyURLSearchParams) => {
-    const params = new URLSearchParams(searchParams);
-    params.set("page", String(Number(page) + 1));
-    console.log("page2");
-  };
+  const router = useRouter();
+  const page = router.query.query as string;
+  const [currentPage, setCurrentPage] = useState(parseInt(page) || 1)
+  const urlApi = `/api/products/?page=${currentPage}&setCurrent`;
 
-  const urlApi = `/api/products/?page=${page}`;
-
-  const fetcher = async (url: string, page = "1") => {
-    const response = await fetch(url);
+  const fetcher = async (url: string, page = 1) => {
+    const response = await fetch(`${url}?page=${page}`);
     const data = await response.json();
-    console.log(page);
 
     if (!response.ok) throw new Error("error fetching");
 
     return data;
   };
+  
   const { data, error, isLoading } = useSWR([urlApi, page], ([url, page]) =>
-    fetcher(url, page),
-  );
+    fetcher(url, parseInt(page)),
+);
+
+  const nextPage = () => {
+    try {
+      const urlObj = new URL(data.next);
+      const page = urlObj.searchParams.get('page') || "1"; 
+      setCurrentPage(parseInt(page));
+      router.push(`/productos?page=${page}`, undefined, { shallow: true });
+      
+    } catch (error) {
+      console.error('Invalid URL:', error);
+    }
+  }
 
   return (
     <ContentMain title="Nuestros Productos">
@@ -41,7 +48,7 @@ const ProductListPage: React.FC = () => {
       ) : (
         <div>
           <ProductGridList products={data.results} />
-          <button onClick={() => nextPage(searchParams)}>Next</button>
+          <button onClick={() => nextPage()}>Next</button>
         </div>
       )}
     </ContentMain>
