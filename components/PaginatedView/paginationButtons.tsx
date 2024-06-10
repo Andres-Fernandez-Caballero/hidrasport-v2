e46497227ec95cbf725601da1f80bfd632ae7180
PabloGabrielDonato <iphone.pablo2002@gmail.com>
2024-06-10 20:08:46 -0300

    Modificacion de banner inicial, y agregado de carrusel de productos destacados

--- components/PaginatedView/paginationButtons.tsx
+++ components/PaginatedView/paginationButtons.tsx
@@ -20,7 +20,7 @@ interface CurrentPageIndiatorProps {
 const PaginationButton = (props: PaginationButtonProps) => (
     <button
         onClick={props.onClick}
-        className="bg-gray-200 hover:bg-gray-300 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
+        className=" text-gray-800 font-semibold py-2 px-4 border">
         <span className="sr-only">{props.text}</span>
         {props.children}
     </button>
@@ -37,18 +37,24 @@ const PaginationMenu = ({ onNext, onPrevius, currentPage, hasNext = false, hasPr
     <menu className="flex items-center gap-4">
         {hasPrevious &&
             <PaginationButton onClick={onPrevius} text="Previous" >
-                <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-7-7 7-7" />
-                </svg>
+                <a className="flex items-center justify-center px-3 h-8 me-3 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                    <svg className="w-3.5 h-3.5 me-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 5H1m0 0 4 4M1 5l4-4"/>
+                    </svg>
+                    Anterior
+                </a>
             </PaginationButton>}
 
         <CurrentPageIndiator currentPage={currentPage} />
 
         {hasNext &&
             <PaginationButton onClick={onNext} text="Next" >
-                <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7" />
-                </svg>
+                <a className="flex items-center justify-center px-3 h-8 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                    Siguiente
+                    <svg className="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                    </svg>
+                </a>
             </PaginationButton>}
     </menu>
 )
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,10 +4,10 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css"; // Import styles for the carousel
 import useSWR from "swr";
 import { SERVER_URL } from "@config/index";
 import Loader from "@components/Loader";
-
 
 interface resultsProps {
   id: number;
@@ -20,11 +20,6 @@ interface dataProps {
   previous: string | null;
   results: resultsProps[];
 }
-
-// FUTURA CARACTERISTICA
-// const imageLoader = ({ src, width, quality }) => {
-//   return `https://example.com/${src}?w=${width}&q=${quality || 75}`
-// }
 
 const items: ImageGridLinkItem[] = [
   {
@@ -59,18 +54,134 @@ interface ImageGridProps {
   items: ImageGridLinkItem[];
 }
 
-const CarouselHome: React.FC<ImageGridProps> = (props) => {
+const CarouselHome: React.FC<ImageGridProps> = () => {
   const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 4,
+    },
     tablet: {
-      breakpoint: { max: 768, min: 464 },
-      items: 1,
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
     },
     mobile: {
       breakpoint: { max: 464, min: 0 },
       items: 1,
     },
-  };  
-  
+  };
+
+  const productosDestacados = [
+    {
+      title_id: 171,
+      title: "Rompevientos Guardavidas Ocean",
+      price: 46899.0,
+      b2b_price: 25911.697500000002,
+      images: [
+        {
+          image: "/static/media/products/ROJA_FRENTE_lAs0yjQ.png",
+        },
+        {
+          image: "/static/media/products/ROJA_ESPALDA_2024.png",
+        },
+      ],
+    },
+    {
+      title_id: 80,
+      title: "Primera Piel Protect UV Guardavidas",
+      price: 32990.0,
+      b2b_price: 18226.975,
+      images: [
+        {
+          image: "/static/media/products/PRIMERA_PIEL_BLANCA_FRENTE_LOGO_ROJO_WEB.png",
+        },
+        {
+          image: "/static/media/products/PRIMERA_PIEL_BLANCA_ESPALDA_2024.png",
+        },
+      ],
+    },
+    {
+      title_id: 80,
+      title: "Primera Piel Protect UV Guardavidas",
+      price: 32990.0,
+      b2b_price: 18226.975,
+      images: [
+        {
+          image: "/static/media/products/BLANCA_ROJA_FRENTE.png",
+        },
+        {
+          image: "/static/media/products/BLANCA_ROJA_ESPALDA.png",
+        },
+      ],
+    },
+    {
+      title_id: 172,
+      title: "Sungon Geo Slim",
+      price: 30199.0,
+      b2b_price: 16684.947500000002,
+      images: [
+        {
+          image: "/static/media/products/TRIANGULOS_AZUL_FRENTE.png",
+        },
+        {
+          image: "/static/media/products/TRIANGULOS_AZUL_ESPALDA.png",
+        },
+      ],
+    },
+    {
+      title_id: 168,
+      title: "Manual de Soporte Vital Básico en Ahogamiento",
+      price: 7299.0,
+      b2b_price: 4032.6975,
+      images: [
+        {
+          image: "/static/media/products/MANUAL_PNG_WEB_zFMl3QJ.png",
+        },
+      ],
+    },
+    {
+      title_id: 164,
+      title: "Short de Baño Clasico Unisex Con suspensor",
+      price: 26399.0,
+      b2b_price: 14585.447500000002,
+      images: [
+        {
+          image: "/static/media/products/AZUL_FRENTE2_Spvfj9i.png",
+        },
+        {
+          image: "/static/media/products/AZUL_ESPALDA_2_nsf6a4l.png",
+        },
+      ],
+    },
+    {
+      title_id: 163,
+      title: "Sungon Classic Guardavidas",
+      price: 30199.0,
+      b2b_price: 16684.947500000002,
+      images: [
+        {
+          image: "/static/media/products/ROJO_FRENTE_HWfquPT.png",
+        },
+        {
+          image: "/static/media/products/ROJO_ESPALDA_NHPAiLF.png",
+        },
+      ],
+    },
+    {
+      title_id: 114,
+      title: "Campera Clásica HS Unisex Guardavidas",
+      price: 45299.0,
+      b2b_price: 25027.697500000002,
+      images: [
+        {
+          image: "/static/media/products/CLASICA_GUARD_2024.png",
+        },
+        {
+          image: "/static/media/products/CLASICA_GUARD_2024_ESPALDA.png",
+        },
+      ],
+    },
+  ];
+
   return (
     <Carousel
       responsive={responsive}
@@ -81,19 +192,22 @@ const CarouselHome: React.FC<ImageGridProps> = (props) => {
       arrows={false}
       className="w-full"
     >
-      {props.items.map((item, index: number) => (
-        <figure className=" relative opacity-90 h-fit" key={index + item.label}>
-          <Image
-            src={item.image}
-            alt={item.label}
-            width={400}
-            height={400}
-            className="rounded-sm"
-          />
-          <h2 className="absolute top-0 p-4  flex justify-center items-center text-lg text-red-100">
-            {item.label}
-          </h2>
-        </figure>
+      {productosDestacados.map((item, index: number) => (
+        <div key={index} className="p-4">
+          <div className="bg-white shadow-md rounded-lg overflow-hidden mb-5 min-h-96">
+            <Image
+              src="https://picsum.photos/300/300"
+              alt={item.title}
+              width={400}
+              height={400}
+              className="object-cover w-full h-56 m-4"
+            />
+            <div className="p-4">
+              <h3 className="text-lg font-bold">{item.title}</h3>
+              <p className="text-gray-500">${item.price.toFixed(2)}</p>
+            </div>
+          </div>
+        </div>
       ))}
     </Carousel>
   );
@@ -101,27 +215,27 @@ const CarouselHome: React.FC<ImageGridProps> = (props) => {
 
 const BannerHome: React.FC<ImageGridProps> = (props) => {
   return (
-    <menu className="grid gap-1 grid-rows-1 grid-cols-4 m-5 h-fit ">
-  {props.items.map((item, index: number) => (
-    <Link
-      href={item.link}
-      className="relative opacity-90 h-fit m-2 border-2"
-      key={index + item.label}
-      style={{ position: 'relative' }}
-    >
-      <Image
-        src={item.image}
-        alt={item.label}
-        width={400}
-        height={400}
-        className="rounded-lg blur-sm "
-      />
-      <h2 className="font-sans font-bold text-4xl text-blue-50 tracking-wide absolute inset-0 flex items-center justify-center p-4 bg-transparent bg-opacity-0 hover:bg-opacity-25 hover:scale-110 ease-in-out duration-300">
-        {item.label}
-      </h2>
-    </Link>
-  ))}
-</menu>
+    <menu className="grid gap-1 grid-rows-1 grid-cols-4 m-5 h-fit">
+      {props.items.map((item, index: number) => (
+        <Link
+          href={item.link}
+          className="relative opacity-90 h-fit m-2 "
+          key={index + item.label}
+          style={{ position: 'relative' }}
+        >
+          <Image
+            src={item.image}
+            alt={item.label}
+            width={400}
+            height={400}
+            className=" blur-sm "
+          />
+          <h2 className="font-sans font-bold text-4xl text-blue-50 tracking-wide absolute inset-0 flex items-center justify-center p-4 bg-transparent bg-opacity-0 hover:bg-opacity-25 hover:scale-110 ease-in-out duration-300">
+            {item.label}
+          </h2>
+        </Link>
+      ))}
+    </menu>
   );
 };
 
@@ -140,12 +254,10 @@ const Home: NextPage = () => {
     `${SERVER_URL}/api/store/site-configuration/`,
     fetcher,
   );
-  if (!data && !error) return <Loader/>;
+  if (!data && !error) return <Loader />;
   if (error) {
-    // guardar el error en un log
     console.error(error);
   }
-
   return (
     <main>
       {/* 
@@ -171,23 +283,49 @@ const Home: NextPage = () => {
       </section>
 
      */}
-      <section
-        style={{
+    <section
+      style={{
         backgroundImage: data && `url(${data.results[0].banner.replace(
           "http://localhost:8000",
-          "https://hidrasport.com.ar",
+          "https://hidrasport.com.ar"
         )})`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-         // ajusta el alto según tus necesidades
-        }}
-        className="h-screen flex items-center justify-center"
-      >
-        {/* Aquí puedes agregar contenido encima de la imagen */}
-        <div className="w-96 p-4 text-center  backdrop-blur-md bg-opacity-30  rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+      }}
+      className="relative h-screen flex items-center justify-center overflow-hidden"
+    >
+      {/* Pseudo-elemento ::before para el fondo desenfocado */}
+      <div className="absolute bg-cover inset-0 bg-center  blur-sm" style={{ backgroundImage: data && `url(${data.results[0].banner.replace(
+          "http://localhost:8000",
+          "https://hidrasport.com.ar"
+        )})` }}></div>
 
-            </div>      
-      </section>
+      {/* Contenido sobre la imagen */}
+      <div className="md:z-10 flex h-screen w-full">
+        <div className="w-1/2 flex flex-col justify-center items-center">
+          {/* Contenido de la columna izquierda (información) */}
+          <div className="rounded-md text-center p-2 m-2  bg-opacity-75 backdrop-blur-md">
+            <h2 className="text-2xl font-bold m-4">Información</h2>
+            <p className="text-center m-4 text-xl text-white">
+              Aquí va la información. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+            <MessageBar
+              messages={[
+                "Envios en toda capital GRATIS",
+                "Devoluciones sin cargo",
+                "Cuotas sin interés 💳",
+                "Más de 1000 productos 🛒 ",
+              ]}
+              delay={5000}
+            />
+            <button type="button" className="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">¡Nuestros productos!</button>
+          </div>
+        </div>
+        <div className="w-1/2 p-4 flex flex-col justify-center items-center">
+          {/* Contenido de la columna derecha (imágenes) */}
+          <img src="https://picsum.photos/500/500" alt="Placeholder" className="mb-4" />
+        </div>
+      </div>
+    </section>
+
 
       <section className="flex">
         <h2 className="sr-only">CATEGORIAS POPULARES</h2>
@@ -198,15 +336,7 @@ const Home: NextPage = () => {
           <BannerHome items={items} />
         </div>
       </section>
-      <MessageBar
-        messages={[
-          "Envios en toda capital GRATIS",
-          "Devoluciones sin cargo",
-          "Cuotas sin interés 💳",
-          "Más de 1000 productos 🛒 ",
-        ]}
-        delay={5000}
-      />
+
       <section className="grid grid-cols-1 md:grid-cols-3 justify-center items-center my-20 space-x-4 m-10">
         
         <article className="flex justify-center gap-2 items-center shadow-2xl bg-slate-200 rounded-xl p-6">        
@@ -239,6 +369,9 @@ const Home: NextPage = () => {
       </section>
       <section className="productos_dest">
         <h2 className="text-2xl text-center font-extrabold">Productos Destacados 🥇</h2>
+
+                
+        <CarouselHome items={items} />
 
         {/* SECCION CARDS */}
         <div className="flex items-center justify-center m-10  container mx-auto ">
