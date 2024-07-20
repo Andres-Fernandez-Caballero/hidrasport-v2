4b5bc63aa8b115147cdc91ecb6cfc4fabf121348
Taameh <arroyotomas1999@gmail.com>
2024-07-19 21:03:14 -0300

    -Update gitIgnore

---
+++ pages/checkout/components/contracts.tsx
@@ -0,0 +1,13 @@
+import IUseCheckout from "app/hooks/useCheckout/interface";
+
+
+export interface CheckoutFormDataProps {
+    checkoutData: IUseCheckout;
+}
+
+export default function EmptyComponent(){
+    return(
+        <>   
+        </>
+    )
+}
