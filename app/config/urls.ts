9006197f1838e0eeec389859caf7698754425ad0
Andres Fernandez <andres.fernandezcaballero@davinci.edu.ar>
2024-07-29 13:30:18 -0300

    update login and register logic

--- app/config/urls.ts
+++ app/config/urls.ts
@@ -1,6 +1,7 @@
 const backendUrl: string = process.env.SERVER_URL || "http://localhost:8000";
 const urls = {
-  login: backendUrl + "/api/accounts/login",
+  login: backendUrl + "/api/accounts/login/",
+  register: backendUrl + "/api/accounts/register/",
   orders: backendUrl + "/api/orders/orders/filter/",
   permission: backendUrl + "/api/accounts/users/get-permission-level/",
 };
--- repositories/user/user.repository.ts
+++ repositories/user/user.repository.ts
@@ -1,19 +1,31 @@
 import urls from '../../app/config/urls';
 import { LoginDto } from "@interfaces/IAuth";
-import { LoginResponse } from "./contracts";
+import { LoginResponse, RegisterResponse } from "./contracts";
 
-export const Login = async(loginData: LoginDto):Promise<LoginResponse> => {
-    console.log(urls.login);
-    const response = await fetch(urls.login, {
+export const Login = async (loginData: LoginDto): Promise<LoginResponse> => {
+    const response = await fetch(`/api/auth/login`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify(loginData)
     });
-
+    
     if (!response.ok) { 
-        throw new Error(response.statusText);
+        const errorText = await response.text();
+        throw new Error(`Error ${response.status}: ${errorText}`);
     }
+
+    return response.json();
+};
+
+export const Register = async(loginData: LoginDto): Promise<RegisterResponse> => {
+    const response = await fetch(`/api/auth/register`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(loginData)
+    });
     return response.json();
 }
