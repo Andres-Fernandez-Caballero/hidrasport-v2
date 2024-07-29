02f351bf011dc5b3fad9a04b2df7af3000edba10
Taameh <arroyotomas1999@gmail.com>
2024-07-29 19:15:58 -0300

    Update. Limpieza y organizacion de footer

--- app/components/layout/contentMain/footer/index.tsx
+++ app/components/layout/contentMain/footer/index.tsx
@@ -2,7 +2,6 @@ import { WHATSAPP_BUSINESS_NUMBER } from "@config/index";
 import {
   FaPhone,
   FaEnvelope,
-  FaClockRotateLeft,
   FaInstagram,
   FaFacebook,
 } from "react-icons/fa6";
@@ -10,14 +9,13 @@ import AFIP from "./Afip";
 
 const About = () => {
   return (
-    <section className="w-full md:w-4/12 px-4 text-black text-center">
-      <h4 className="mb-4">Sobre nosotros</h4>
+    <section className="w-full md:w-4/12 px-4  text-center">
       <ul className="mb-4">
-        <li className="flex items-center mb-2">
+        <li className="flex items-center mb-2 justify-center">
           <FaPhone className="mr-2" width={50} />
           <a href="#"> Telefono: +{WHATSAPP_BUSINESS_NUMBER} </a>
         </li>
-        <li className="flex items-center mb-2">
+        <li className="flex items-center mb-2 justify-center">
           <FaEnvelope className="mr-2" width={50} />
           <a href="#">Email: </a>
         </li>
@@ -26,30 +24,15 @@ const About = () => {
   );
 };
 
-const AtencionCliente = () => {
-  return (
-    <section className="w-full md:w-4/12 px-4 text-black text-center">
-      <h4 className="mb-4">Atención al Cliente</h4>
-      <ul className="mb-4">
-        <li className="flex items-center mb-2">
-          <FaClockRotateLeft width={50} className="mr-2" />
-          <a href="#">Horario de Atención: </a>
-        </li>
-      </ul>
-    </section>
-  );
-};
-
 const RedesSociales = () => {
   return (
-    <section className="w-full md:w-4/12 px-4 text-black text-center">
-      <h4 className="mb-4">Redes Sociales</h4>
+    <section className="w-full md:w-4/12 px-4  text-center">
       <ul className="mb-4">
-        <li className="flex items-center mb-2">
+        <li className="flex items-center justify-center mb-2">
           <FaFacebook width={50} className="mr-2" />
           <a href="http://facebook.com">Facebook</a>
         </li>
-        <li className="flex items-center mb-2">
+        <li className="flex items-center justify-center mb-2">
           <FaInstagram width={50} className="mr-2" />
           <a href="http://instagram.com">Instagram</a>
         </li>
@@ -60,7 +43,7 @@ const RedesSociales = () => {
 
 const AFIPColum = () => {
   return (
-    <section className="w-full md:w-4/12 px-4 flex justify-center mx-auto text-black">
+    <section className="w-full md:w-4/12 px-4 flex justify-center mx-autok">
       <div className="flex flex-col items-center justify-center text-center">
         <AFIP />
         <span className="flex items-center flex-col">Hidrasport ®2023</span>
@@ -70,11 +53,10 @@ const AFIPColum = () => {
 };
 
 const Footer = () => (
-  <footer className="relative bg-cover bg-center text-white bg-gray-100 flex justify-center">
+  <footer className="relative bg-cover bg-center text-white bg-gray-950 flex justify-center">
     <div className="absolute inset-0 backdrop-blur-sm"></div>
     <div className="relative container mx-auto flex flex-wrap py-8">
       <About />
-      <AtencionCliente />
       <RedesSociales />
       <AFIPColum />
     </div>
