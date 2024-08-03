1ee93733f2a665de4ec3fa0d77413f51b58a0803
Andres Fernandez <andres.fernandezcaballero@davinci.edu.ar>
2024-08-03 19:50:22 -0300

    change color of icons in loader and empty cart

--- app/components/common/Loader/index.tsx
+++ app/components/common/Loader/index.tsx
@@ -1,6 +1,6 @@
 const Loader = () => (
   <section className="flex flex-col items-center justify-center min-h-screen text-center bg-gray-100">
-    <i className="text-7xl fa-solid fa-person-swimming fa-beat text-red-400"></i>
+    <i className="text-7xl fa-solid fa-person-swimming fa-beat text-gray-800"></i>
   </section>
 );
 export default Loader;
--- app/components/layout/cart/CartEmptyLayout.tsx
+++ app/components/layout/cart/CartEmptyLayout.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const CartEmptyLayout = () => (
     <div className="flex flex-col items-center justify-center min-h-screen text-center bg-gray-100">
-       <i className="text-7xl fa-solid fa-cart-shopping text-red-400"></i>
+       <i className="text-7xl fa-solid fa-cart-shopping text-gray-800       "></i>
         <h2 className="mt-8 text-2xl font-semibold text-gray-900">Tu carrito está vacío</h2>
         <p className="mt-2 text-lg text-gray-700">Parece que aún no has añadido ningún artículo a tu carrito.</p>
         <Link href="/productos" >Conoce nuestros productos</Link>
