56e1b9f69c8d11f99ad80fd9ec0eb54ea5f1f410
Andres Fernandez <andres.fernandezcaballero@davinci.edu.ar>
2024-08-10 11:30:55 -0300

    add siteConfig store

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,19 +8,28 @@ import "react-multi-carousel/lib/styles.css";
 import CardProile from "@components/common/cards/cardProfile";
 import { PrimeReactProvider} from 'primereact/api';
 import 'primereact/resources/themes/lara-light-cyan/theme.css';
+import useSiteConfigStore from "@store/siteConfig/useSiteConfigStore";
         
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   const { fetchCart } = useCartStore();
+  const {fetchSiteConfig} = useSiteConfigStore();
   useEffect(() => {
     try {
+      fetchSiteConfig().catch(error => {
+        toast.error("Error al cargar la información del sitio");
+        console.log("error", error);
+      });
+
       fetchCart();
+
     } catch (error) {
       toast.error((error as Error).message);
       console.log("error", error);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [CardProile]);
+  
   return (
     <Layout>
       <PrimeReactProvider>
