da482d23ca5c6bc9a9aab4b5a94649ee216a595a
Tomás Arroyo <97720459+Taameh@users.noreply.github.com>
2024-08-24 20:18:30 -0300

    -Fix. Correccion de modal para tapar todo con el background

--- app/components/layout/authmodal/index.tsx
+++ app/components/layout/authmodal/index.tsx
@@ -7,28 +7,26 @@ const AuthModal = () => {
 
   return (
     <menu
-      className="relative z-100"
+      className="relative z-50"
       aria-labelledby="modal-title"
       role="dialog"
       aria-modal="true"
     >
-      <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+      <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity z-40"></div>
 
-      <div className="fixed inset-0 z-10 overflow-y-auto">
-        <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-          <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-            <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
-              <div className="flex justify-between z-100  ">
-                {tab === "login" ? <Login /> : <Register />}
-                <nav>
-                  <button
-                    onClick={closeModal}
-                    className="border p-1 w-8 h-8 bg-slate-50  rounded-full text-gray-700 shadow-md hover:border-gray-800"
-                  >
-                    <i className="fa-solid fa-xmark"></i>
-                  </button>
-                </nav>
-              </div>
+      <div className="fixed inset-0 z-50 flex items-center justify-center overflow-y-auto">
+        <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:w-full sm:max-w-lg">
+          <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+            <div className="flex justify-between z-50">
+              {tab === "login" ? <Login /> : <Register />}
+              <nav>
+                <button
+                  onClick={closeModal}
+                  className="border p-1 w-8 h-8 bg-slate-50 rounded-full text-gray-700 shadow-md hover:border-gray-800"
+                >
+                  <i className="fa-solid fa-xmark"></i>
+                </button>
+              </nav>
             </div>
           </div>
         </div>
@@ -38,3 +36,5 @@ const AuthModal = () => {
 };
 
 export default AuthModal;
+
+
