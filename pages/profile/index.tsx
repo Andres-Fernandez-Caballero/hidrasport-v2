af1a652b21968a5994c8583734fa2f01ba728d55
Tomás Arroyo <97720459+Taameh@users.noreply.github.com>
2024-08-24 20:23:24 -0300

    -Update. Estetica de la seccion de usuario

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -6,20 +6,19 @@ import useCartStore from "@store/cart/useCartStore";
 import useOrderStore from "@store/order/useOrderStore";
 import Image from "next/image";
 import Link from "next/link";
-import { useRouter } from 'next/navigation'
+import { useRouter } from 'next/navigation';
 import { DataScroller } from "primereact/datascroller";
 import { useEffect } from "react";
-
+import styles from "./styles.module.css"
 
 const Profile = () => {
   const { userSession, logout, isLogedIn } = useAuthStore();
   const { cartData } = useCartStore();
   const { orders } = useOrderStore();
-  const router = useRouter()
+  const router = useRouter();
 
   console.log(isLogedIn());
   console.log(userSession);
-
 
 
   useEffect(() => {
@@ -39,75 +38,67 @@ const Profile = () => {
   }
 
   return (
-    <main className="">
-    <header>
-      <h1 className="text-gray-700 font-bold text-4xl leading-10 text-justify px-12 invisible">
-        Perfil de usuario
-      </h1>
-    </header>
-    <section className="grid gap-4 md:grid-cols-1 lg:grid-cols-3">
-      <div className="w-full md:max-w-full lg:max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 m-auto">
-        <div className="flex flex-col items-center p-5">
-          <Image className="w-24 h-24 mb-3 rounded-full shadow-lg" src="/images/avatar.jpg" alt="Avatar del usuario logueado" width={400} height={400} />
-          <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">{userSession.username}</h5>
-          <span className="text-sm text-gray-500 dark:text-gray-400">{userSession.email}</span>
-          <menu className="flex mt-6">
-            <button
-              className="bg-blue-500 text-white border border-black rounded-tl-xl p-2"
-              onClick={() => alert("caracteristica en desarrollo 🚧")}
-            >
-              <span className="flex gap-4 items-center">Editar datos</span>
-            </button>
-            <button className="border border-gray-800 p-4 rounded-br-xl" onClick={logout}>
-              <span className="flex gap-4 items-center">Logout</span>
-            </button>
-          </menu>
-        </div>
-      </div>
-      <div className="w-full md:max-w-full lg:max-w-md p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700 m-auto">
-        <header className="flex justify-between items-center p-4 bg-gray-100">
-          <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white">Carrito</h5>
-          <div>
-            <Link href="/carrito" className="text-blue-500 hover:text-blue-700">
-              Ir a carrito <span className="pi pi-shopping-cart" />
-            </Link>
+    <main className="p-4 w-full lg:min-h-[90vh] flex items-center">
+      <section className="grid gap-4 grid-cols-1 lg:grid-cols-4 w-full">
+        <div className="col-span-1 lg:row-span-2 w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 flex flex-col">
+          <div className="flex flex-col items-center p-5 h-full">
+            <Image className="w-24 h-24 mb-3 rounded-full shadow-lg" src="/images/avatar.jpg" alt="Avatar del usuario logueado" width={400} height={400} />
+            <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">{userSession.username}</h5>
+            <span className="text-sm text-gray-500 dark:text-gray-400">{userSession.email}</span>
+            <menu className={styles.doubleButton}>
+              <button
+                className={styles.editButton}
+                onClick={() => alert("caracteristica en desarrollo 🚧")}
+              >
+                <span className="flex items-center">Editar datos</span>
+              </button>
+              <button className={styles.logoutButton} onClick={logout}>
+                Logout
+              </button>
+            </menu>
           </div>
-        </header>
-        <div className="flow-root overflow-x-auto">
-          <div className="h-52 overflow-x-auto w-full max-w-xl">
-            <ul role="list" className="divide-y divide-gray-200 dark:divide-gray-700">
-              <FlatList
-                data={cartData}
-                direction="vertical"
-                renderItem={(item) => <CartItemCard item={item} />}
-                renderEmptyList={() => <p>No hay productos en el carrito</p>}
+        </div>
+        <div className="col-span-3 lg:col-span-2 h-full w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 flex flex-col">
+          <header className="flex justify-between items-center p-4 bg-gray-100">
+            <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white">Carrito</h5>
+            <div>
+              <Link href="/carrito" className="text-blue-500 hover:text-blue-700">
+                Ir a carrito <span className="pi pi-shopping-cart" />
+              </Link>
+            </div>
+          </header>
+          <div className="flex-grow overflow-x-auto">
+            <div className="h-96 overflow-x-auto w-full max-w-full">
+              <ul role="list" className="divide-y divide-gray-200 dark:divide-gray-700">
+                <FlatList
+                  data={cartData}
+                  direction="vertical"
+                  renderItem={(item) => <CartItemCard item={item} />}
+                  renderEmptyList={() => <p>No hay productos en el carrito</p>}
+                />
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="col-span-3 lg:col-span-1 w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 flex flex-col">
+          <header className="flex justify-between items-center p-4 bg-gray-100">
+            <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white">Ordenes</h5>
+          </header>
+          <div className="flex-grow overflow-x-auto">
+            <div className="h-52 overflow-x-auto w-full max-w-xl">
+              <DataScroller
+                value={orders}
+                itemTemplate={(item) => <OrderItem item={item} />}
+                rows={5}
+                inline
+                scrollHeight="100%"
               />
-            </ul>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="w-full md:max-w-full lg:max-w-md p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700 m-auto">
-        <header className="flex justify-between items-center p-4 bg-gray-100">
-          <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white">Ordenes</h5>
-          <div>
-            {/* <Link href="/carrito" className="text-blue-500 hover:text-blue-700">Ir a carrito <span className="pi pi-shopping-cart" /></Link> */}
-          </div>
-        </header>
-        <div className="flow-root overflow-x-auto">
-          <div className="h-52 overflow-x-auto w-full max-w-xl">
-            <DataScroller
-              value={orders}
-              itemTemplate={(item) => <OrderItem item={item} />}
-              rows={5}
-              inline
-              scrollHeight="100%"
-            />
-          </div>
-        </div>
-      </div>
-    </section>
-  </main>
-  
+      </section>
+    </main>
+
   )
 };
 
